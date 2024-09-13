4a37ac9abadfb8a55c39c3177b4482652375b7fa
MValerio08 <marcos88.ramos@gmail.com>
2024-09-13 09:17:55 -0400

    insercion de datos en php

--- estudiante.php
+++ estudiante.php
@@ -2,12 +2,15 @@
 
   include_once "conexion.php";
   class Estudiante extends Conexion{
+
+    //Declaracion de variables
     public $ci;
     public $nombre;
     public $ap_paterno;
     public $ap_materno;
     public $numero_telf;
 
+    //Funcion para visualizar los datos
     public static function ver(){
       $cone = new Conexion();
       $cone->conectar();
@@ -20,6 +23,14 @@
       };
       return json_encode($estudiantes);
 
+    }
+
+    //Funcion para la insercion de datos
+    public function insertar(){
+      $this->conectar();
+      $insertar = mysqli_prepare($this->con,"insert into alumnos values(?,?,?,?,?)");
+      $insertar->bind_param("issss",$this->ci,$this->nombre,$this->ap_paterno,$this->ap_materno,$this->numero_telf);
+      $insertar->execute();
     }
   }
 
--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php 
   include_once "estudiante.php";
-  $a = Estudiante::ver();
-  echo ($a);
+  $a = new Estudiante();
+  $a->ci = 136;
+  $a->nombre = "alvaro";
+  $a->ap_paterno = "Colque";
+  $a->ap_materno = "Zubieta";
+  $a->numero_telf = "65897634";
+  $a->insertar();
+  echo "Datos insertados correctamente";
+  
 
+
+  //echo ($a);
+  
 ?>
